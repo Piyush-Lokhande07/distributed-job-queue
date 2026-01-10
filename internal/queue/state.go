c431b4d579d23c14c99a3e2a8250efac2870aab2
Piyush_Lokhande <piyushkailas77@gmail.com>
2026-01-10 16:00:34 +0530

    Workers connected with Redis

--- internal/queue/state.go
+++ internal/queue/state.go
@@ -1,1 +1,44 @@
 package queue
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/models"
+)
+
+func UpdateStatus(id int, newStatus string)error {
+
+	jobIDStr := strconv.Itoa(id)
+
+	hashKey := "job:" + jobIDStr
+
+	jsonData, err := RDB.HGet(Ctx, hashKey, "data").Result()
+
+	if err != nil {
+		return fmt.Errorf("Error getting json data\n")
+	}
+
+	var job models.Job
+	if err:= json.Unmarshal([]byte(jsonData), &job);err!=nil{
+		return fmt.Errorf("Error while unmarshal the json data")
+	}
+
+	job.Status = newStatus
+	job.UpdatedAt = time.Now()
+
+	updatedData,err:= json.Marshal(job)
+
+	if err!=nil{
+		return fmt.Errorf("Failed to marshal updated data")
+	}
+
+	return RDB.HSet(Ctx, hashKey,map[string]interface{}{
+		"data":string(updatedData),
+		"status":newStatus,
+
+	}).Err()
+
+}
--- internal/worker/engine.go
+++ internal/worker/engine.go
@@ -2,8 +2,12 @@ package worker
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 	"time"
+
+	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/models"
+	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/queue"
 )
 
 func StartWorker(ID int, wg *sync.WaitGroup) {
@@ -11,10 +15,39 @@ func StartWorker(ID int, wg *sync.WaitGroup) {
 
 	fmt.Printf("[Worker %d] Started and waiting for jobs...\n", ID)
 
-	fmt.Printf("[Worker %d] Processing Job \n", ID)
+	for {
 
-	time.Sleep(2 * time.Second)
+		result, err := queue.RDB.BRPop(queue.Ctx, 0, "job_queue").Result()
+		if err != nil {
+			fmt.Printf("[Worker %d] Error while popping job %v\n", ID, err)
+		}
+		jobID := result[1]
 
-	fmt.Printf("[Worker %d] finished Job \n", ID)
+		idInt, err := strconv.Atoi(jobID)
+		if err != nil {
+			fmt.Printf("Error converting ID: %v\n", err)
+			continue
+		}
+
+		fmt.Printf("[Worker %d] grabbed the job:%s\n", ID, jobID)
+
+		err = queue.UpdateStatus(idInt, models.StateInProgress)
+		if err != nil {
+
+			fmt.Printf("[Worker %d] Error updating status %v\n", ID, err)
+		}
+
+		fmt.Printf("[Worker %d] Processing the Job %s\n", ID, jobID)
+		time.Sleep(2 * time.Second)
+
+		err = queue.UpdateStatus(idInt, models.StateCompleted)
+
+		if err != nil {
+
+			fmt.Printf("[Worker %d] Error updating status %v\n", ID, err)
+		}
+
+		fmt.Printf("[Worker %d] finished Job \n", ID)
+	}
 
 }
