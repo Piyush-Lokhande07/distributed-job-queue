0877cda9e7d095d8b32aa4c2b2051fa9ba88774f
Piyush_Lokhande <piyushkailas77@gmail.com>
2026-01-12 15:17:34 +0530

    Added graceful shutdown for workers and API

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/api"
 	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/queue"
@@ -15,34 +18,65 @@ import (
 func main() {
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-
 	slog.SetDefault(logger)
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	err := queue.Connect()
 
 	if err != nil {
-		fmt.Println("Error connecting to redis")
+		slog.Error("Error connecting to Redis", "error", err)
+		os.Exit(1)
 	}
-	fmt.Println(" Connected to Redis")
+	slog.Info("Connected to Redis")
 
 	var wg sync.WaitGroup
 
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go worker.StartWorker(i, &wg)
+		go worker.StartWorker(ctx, i, &wg)
 	}
 
 	http.HandleFunc("/jobs", api.HandleCreateJob)
 	http.HandleFunc("/metrics", api.GetMetrics)
 	http.HandleFunc("/status", api.GetJobStatus)
 
-	// fmt.Println("Server running on port:[8080]")
-	slog.Info("Server running","port","8080")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Server failed %v\n", err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: nil,
 	}
 
+	go func() {
+		slog.Info("Server running", "port", 8080)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			slog.Error("HTTP Server error", "error", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan
+
+	slog.Info("Shutdown signal recieved. Starting graceful exit...")
+
+	shutDownContext, shutDownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutDownCancel()
+
+	if err:=server.Shutdown(shutDownContext);err!=nil{
+		slog.Error("API server forced to close","error",err)
+	}else{
+		slog.Info("API server closed gracefully.")
+	}
+
+	slog.Info("Signaling workers to stop..")
+
+	cancel()
+
 	wg.Wait()
+	if err := queue.RDB.Close(); err != nil {
+		slog.Error("Error closing Redis connection", "error", err)
+	}
+	slog.Info("System stopped gracefully!")
 
 }
--- internal/worker/engine.go
+++ internal/worker/engine.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"strconv"
@@ -9,9 +10,10 @@ import (
 
 	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/models"
 	"github.com/Piyush-Lokhande07/distributed-job-queue/internal/queue"
+	"github.com/redis/go-redis/v9"
 )
 
-func StartWorker(ID int, wg *sync.WaitGroup) {
+func StartWorker(ctx context.Context, ID int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	workerLog := slog.With("worker_id", ID)
@@ -19,57 +21,64 @@ func StartWorker(ID int, wg *sync.WaitGroup) {
 
 	for {
 
-		result, err := queue.RDB.BRPop(queue.Ctx, 0, "job_queue").Result()
-		if err != nil {
-			workerLog.Error("Error while popping job","error",err)
-			continue
-		}
-		jobID := result[1]
+		select {
+		case <-ctx.Done():
+			workerLog.Info("Worker shutting down")
+			return
+		default:
+			result, err := queue.RDB.BRPop(queue.Ctx, time.Second, "job_queue").Result()
+			if err != nil {
+				if err == redis.Nil {
+					continue
+				}
+				workerLog.Error("Error while popping job", "error", err)
+				continue
+			}
+			jobID := result[1]
 
-		idInt, err := strconv.Atoi(jobID)
-		if err != nil {
-			workerLog.Error("Error converting ID","error",err)
-			continue
-		}
-
-		err = PerformWork(ID, idInt, workerLog)
-
-		if err != nil {
-
-			currentRetries, _ := queue.HandleFailure(idInt)
-			jobLog:= workerLog.With("job_log",idInt)
-
-		
-
-			if currentRetries <= 3 {
-				delay := time.Duration(1<<uint(currentRetries)) * time.Second
-				jobLog.Warn("Job failed, scheduling retry", "delay",delay.String())
-
-				go func(id string, d time.Duration) {
-					time.Sleep(d)
-					err := queue.RDB.LPush(queue.Ctx, "job_queue", id).Err()
-
-					if err != nil {
-						jobLog.Error("CRITICAL: Failed to re-queue Job","error",err)
-					}
-
-				}(jobID, delay)
-
-			} else {
-				jobLog.Info("Permanent job failure")
+			idInt, err := strconv.Atoi(jobID)
+			if err != nil {
+				workerLog.Error("Error converting ID", "error", err)
+				continue
 			}
 
-			continue
+			err = PerformWork(ID, idInt, workerLog)
+
+			if err != nil {
+
+				currentRetries, _ := queue.HandleFailure(idInt)
+				jobLog := workerLog.With("job_log", idInt)
+
+				if currentRetries <= 3 {
+					delay := time.Duration(1<<uint(currentRetries)) * time.Second
+					jobLog.Warn("Job failed, scheduling retry", "delay", delay.String())
+
+					go func(id string, d time.Duration) {
+						time.Sleep(d)
+						err := queue.RDB.LPush(queue.Ctx, "job_queue", id).Err()
+
+						if err != nil {
+							jobLog.Error("CRITICAL: Failed to re-queue Job", "error", err)
+						}
+
+					}(jobID, delay)
+
+				} else {
+					jobLog.Info("Permanent job failure")
+				}
+
+				continue
+
+			}
 
 		}
-
 	}
 
 }
 
-func PerformWork(wId int, jId int,  baseLog *slog.Logger) error {
+func PerformWork(wId int, jId int, baseLog *slog.Logger) error {
 
-	jobLog:= baseLog.With("job_id",jId)
+	jobLog := baseLog.With("job_id", jId)
 
 	if time.Now().UnixNano()%2 == 0 {
 		jobLog.Debug("Simulated error triggered")
@@ -90,7 +99,7 @@ func PerformWork(wId int, jId int,  baseLog *slog.Logger) error {
 	if err != nil {
 		jobLog.Error("Error updating status for Job")
 	}
-	
+
 	jobLog.Info("Finished Job!")
 	time.Sleep(2 * time.Second)
 	return nil
